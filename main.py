89f4966b71ff624f1d431137c38a132ba73c421a
Rexxic <cedricseiz@gmail.com>
2025-04-11 08:59:25 +0200

    Improved Calulation logic

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from model import DrugSet
+from model import Drug, DrugSet
 from pyvis.network import Network
 import re
 
@@ -32,7 +32,42 @@ Available commands:
         - Exit the program.
     """
     print(help_text)
+    
+def calc_cost(drug_set:DrugSet, node:Drug):
+    cost = 0
+    while not node.is_base():
+        for ingredient, child in node.mixed_with.items():
+            if child != node.name:
+                cost += drug_set.ingredients[ingredient]
+                node = drug_set.drugs[child]
+                break
+    return cost + node.value
 
+
+def render(drug_set:DrugSet):
+    net = Network(height="1080", directed=True, select_menu=True)
+    for name, drug in drug_set.drugs.items():
+        value_inc = drug.value - calc_cost(drug_set, drug)
+        net.add_node(
+            name,
+            title=f"Value: {drug.value}$\nImprovement: {value_inc}$",
+            value=value_inc,
+            color="#ff3399" if drug.is_base() else "#669900",
+        )
+        if drug_set.render_ingredients:
+            for ingredient in drug.creates_with:
+                net.add_node(ingredient, color="#0066ff")
+    for name, drug in drug_set.drugs.items():
+        for ingredient, product in drug.creates_with.items():
+            ingredient_value = drug_set.get_ingredient(ingredient)
+            net.add_edge(
+                name, product, title=f"{ingredient}: {ingredient_value}$"
+            )
+            if drug_set.render_ingredients:
+                net.add_edge(ingredient, product)
+    net.toggle_physics(True)
+    net.show_buttons(filter_=["physics"])
+    net.show("nodes.html", notebook=False)
 
 def main():
     """
@@ -54,7 +89,7 @@ def main():
 
         # Parse all command line args with respect for quoted strings composited from mutliple words.
         try:
-            parts = [
+            args = [
                 str(s).replace('"', "")
                 for s in re.findall(r'"[^"]*"|[\S]+', command_input)
             ]
@@ -62,36 +97,36 @@ def main():
             print(f"Error parsing command: {e}")
             continue
 
-        if not parts:
+        if not args:
             continue
 
-        command = str(parts[0]).lower()
+        command = str(args[0]).lower()
 
         if command == "help":
             print_help()
 
         elif command == "add_drug":
-            if len(parts) < 2:
+            if len(args) < 2:
                 print(
                     'Usage: add_drug "drug name" [value] ["base drug name" "ingredient"]'
                 )
                 continue
 
-            name = parts[1]
+            name = args[1]
             value = 0
             base = ""
             ingredient = ""
 
-            if len(parts) >= 3:
+            if len(args) >= 3:
                 try:
-                    value = int(parts[2])
+                    value = int(args[2])
                 except ValueError:
                     print("Value must be an integer.")
                     continue
-            if len(parts) == 5:
-                base = parts[3]
-                ingredient = parts[4]
-            elif len(parts) not in (2, 3):
+            if len(args) == 5:
+                base = args[3]
+                ingredient = args[4]
+            elif len(args) not in (2, 3):
                 print(
                     'Usage: add_drug "drug name" [value] ["base drug name" "ingredient"]'
                 )
@@ -101,10 +136,10 @@ def main():
             print(f"Drug '{name}' added/updated.")
 
         elif command == "get_drug":
-            if len(parts) != 2:
+            if len(args) != 2:
                 print('Usage: get_drug "drug name"')
                 continue
-            name = parts[1]
+            name = args[1]
             drug = drug_set.get_drug(name)
             if drug:
                 print(drug.model_dump())
@@ -112,38 +147,17 @@ def main():
                 print(f"Drug '{name}' not found.")
 
         elif command == "save":
-            if len(parts) == 2:
-                drug_set.save_to_file(parts[1])
+            if len(args) == 2:
+                drug_set.save_to_file(args[1])
             else: drug_set.save_to_file()
 
         elif command == "load":
-            if len(parts) == 2:
-                drug_set = DrugSet.load_from_file(parts[1])
+            if len(args) == 2:
+                drug_set = DrugSet.load_from_file(args[1])
             else: drug_set = DrugSet.load_from_file()
 
         elif command == "render":
-            net = Network(height="1080", directed=True, select_menu=True)
-            for name, drug in drug_set.drugs.items():
-                net.add_node(
-                    name,
-                    title=f"{drug.value}$",
-                    value=drug.value,
-                    color="#ff3399" if drug.is_base() else "#669900",
-                )
-                if drug_set.render_ingredients:
-                    for ingredient in drug.creates_with:
-                        net.add_node(ingredient, color="#0066ff")
-            for name, drug in drug_set.drugs.items():
-                for ingredient, product in drug.creates_with.items():
-                    ingredient_value = drug_set.get_ingredient(ingredient)
-                    net.add_edge(
-                        name, product, title=f"{ingredient}: {ingredient_value}$"
-                    )
-                    if drug_set.render_ingredients:
-                        net.add_edge(ingredient, product)
-            net.toggle_physics(True)
-            net.show_buttons(filter_=["physics"])
-            net.show("nodes.html", notebook=False)
+            render(drug_set)
 
         elif command == "toggle_ingredients":
             drug_set.render_ingredients = not drug_set.render_ingredients
